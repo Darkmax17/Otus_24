c3a6e5d083ef3827bc6d66e444fc338bc4b0f66f
bor1sor <maximchernov2018@gmail.com>
2025-01-16 15:55:19 +0300

    home work 4 vers.2 добавил принцип "максимально поровну" с помощью "distribute_books"

--- files/hw4.py
+++ files/hw4.py
@@ -18,43 +18,53 @@ def read_books(filename):
 def read_users(filename):
     with open(filename, mode='r', encoding='utf-8') as file:
         users = json.load(file)
-    print(f"Количество прочитанных пользователей: {len(users)}")  # Добавление логирования
-    return users
 
+    filtered_users = []  # Новый список для хранения отфильтрованных пользователей
+    for user in users:
+        if all(key in user for key in ['name', 'gender', 'address', 'age']):
+            filtered_user = {
+                'name': user['name'],
+                'gender': user['gender'],
+                'address': user['address'],
+                'age': user['age']
+            }
+            filtered_users.append(filtered_user)
+
+    print(f"Количество прочитанных пользователей: {len(filtered_users)}")  # Добавление логирования
+    return filtered_users
 
 
 # Распределение книг между пользователями
 def distribute_books(books, users):
     num_users = len(users)
-    books_per_user = math.ceil(len(books) / num_users)
+    total_books = len(books)
 
-    result = []  # Новый список для хранения итоговых пользователей
+    # Определяем, сколько книг каждый пользователь должен получить
+    base_books_per_user = total_books // num_users
+    extra_books_count = total_books % num_users  # Дополнительные книги, которые нужно распределить
 
-    for i, user in enumerate(users):
-        # Создание словаря для пользователя с необходимыми полями
-        user_data = {
-            "name": user['name'],
-            "gender": user['gender'],
-            "address": user['address'],
-            "age": user['age'],
-            "books": []
-        }
+    result = [dict(user) for user in users]  # Копируем данные о пользователях
 
-        # Выбор книг для пользователя
-        assigned_books = books[i * books_per_user:(i + 1) * books_per_user]
+    # Обнуляем список книг у каждого пользователя
+    for user in result:
+        user['books'] = []
 
-        # Форматирование книг в нужный вид
-        user_data['books'] = [
-            {
-                "title": book['Title'],
-                "author": book['Author'],
-                "pages": book['Pages'],
-                "genre": book['Genre']
-            }
-            for book in assigned_books
-        ]
+    # Индекс для текущей книги
+    book_index = 0
 
-        result.append(user_data)  # Добавление пользователя в итоговый список
+    for i in range(num_users):
+        # Определяем количество книг для текущего пользователя
+        books_for_user = base_books_per_user + (1 if i < extra_books_count else 0)
+
+        for _ in range(books_for_user):
+            if book_index < total_books:
+                result[i]['books'].append({
+                    "title": books[book_index]['Title'],
+                    "author": books[book_index]['Author'],
+                    "pages": books[book_index]['Pages'],
+                    "genre": books[book_index]['Genre']
+                })
+                book_index += 1
 
     return result
 
